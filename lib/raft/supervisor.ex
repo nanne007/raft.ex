a231d6196524aa605c6d2939e3bd3ceeade2efd7
Cao Jiafeng <funfriendcjf@gmail.com>
2016-05-24 21:00:02 +0800

    fix args

--- lib/raft/supervisor.ex
+++ lib/raft/supervisor.ex
@@ -2,7 +2,7 @@ defmodule Raft.Supervisor do
   use Supervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, name: __MODULE__)
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
 
